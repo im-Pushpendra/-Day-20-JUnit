08d343d77da7c596963125d8ac7c932d0c87db31
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 21:21:55 +0530

    Added: JUnit User Registration

---
+++ userRegistration.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day20.UC_Day_20_JUnit;
+
+public class userRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("User\r\n"
+				+ "Registration\r\n"
+				+ "Problem");
+
+	}
+
+}
